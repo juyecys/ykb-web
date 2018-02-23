0ceaad0c3113189031f942e06f7bc49fe684108e
cysJeysine <yaojiaixn@chengyisheng.com.cn>
2018-02-23 17:21:42 +0800

    fix bug

--- ins-admin/src/main/java/cn/com/yikangbao/api/admin/channelgroup/PrivateAdminChannelGroupController.java
+++ ins-admin/src/main/java/cn/com/yikangbao/api/admin/channelgroup/PrivateAdminChannelGroupController.java
@@ -5,6 +5,8 @@ import cn.com.yikangbao.entity.channelgroup.ChannelGroup;
 import cn.com.yikangbao.entity.channelgroup.ChannelGroupDTO;
 import cn.com.yikangbao.entity.common.Page;
 import cn.com.yikangbao.service.channelgroup.ChannelGroupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +26,15 @@ public class PrivateAdminChannelGroupController {
     @Autowired
     private ChannelGroupService channelGroupService;
 
+    private static final Logger logger = LoggerFactory.getLogger(PrivateAdminChannelGroupController.class);
+
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResponseEntity<ApiResult> saveOrUpdateChannelGroup(@RequestBody ChannelGroup channelGroup) {
-        channelGroup = channelGroupService.createOrUpdate(channelGroup);
+        try {
+            channelGroup = channelGroupService.createOrUpdate(channelGroup);
+        } catch (Exception e) {
+            logger.debug("error: {}", e);
+        }
         return new ResponseEntity<>(ApiResult.success(channelGroup), HttpStatus.OK);
     }
 
